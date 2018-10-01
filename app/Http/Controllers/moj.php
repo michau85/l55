598619475fe73e3abc353a08ec692536933920de
majk19 <majk19@tlen.pl>
2018-10-01 10:27:09 +0200

    inint

---
+++ app/Http/Controllers/moj.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class moj extends Controller
+{
+    public function l55()
+    {
+        return 'l55';
+    }
+
+    public function vue()
+    {
+        return view('vue');
+    }
+}
---
+++ resources/views/vue.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app') 
+@section('content')
+<div class="container">
+    <h1>Laravel 5.5</h1>
+    <ex></ex>
+</div>
+@endsection
